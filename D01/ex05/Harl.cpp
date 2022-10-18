6de04da1809a0414d76a17eacce7f679d20c3cc7
Ayoub Anjaimi <aanjaimi@e1r3p3.1337.ma>
2022-10-18 16:35:03 +0100

    D01:done

--- D01/ex05/Harl.cpp
+++ D01/ex05/Harl.cpp
@@ -5,28 +5,27 @@ void    msg()
     std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
 
-// int set_level(std::string level)
-// {
-//     if (level = "debug")
-// }
-
 void    Harl::debug(void)
 {
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl << "I really do!" << std::endl;
 }
 
 void    Harl::info(void)
 {
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money." << std::endl << "You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
 }
 
 void    Harl::warning(void)
 {
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free." << std::endl << "I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void    Harl::error(void)
 {
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -49,16 +48,29 @@ void    Harl::complain(std::string level)
         &Harl::error
     };
 
-    for (int i = 0;i < 4;i++)
+    int i = 0;
+    for (;i < 4 && in[i] != level;i++)
     {
-        if (in[i] == level)
-        {
-            std::cout << "[ " << level << " ]" << std::endl;
-            (this->*funcs[i])();
-            return ;
-        }
+        
     }
-    msg();
+    switch(i)
+    {
+        case 0:
+            (this->*funcs[0])();
+            break ;
+        case 1:
+            (this->*funcs[1])();
+            break ;
+        case 2:
+            (this->*funcs[2])();
+            break ;
+        case 3:
+            (this->*funcs[3])();
+            break ;
+        default:
+            msg();
+            break ;
+    }
 }
 
 Harl::Harl()
--- D01/ex06/Harl.cpp
+++ D01/ex06/Harl.cpp
@@ -7,22 +7,26 @@ void    msg()
 
 void    Harl::debug(void)
 {
-    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl << "I really do!" << std::endl;
+    std::cout << "[ DEBUG ]" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl << "I really do!" << std::endl << std::endl;
 }
 
 void    Harl::info(void)
 {
-    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl << "You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+    std::cout << "[ INFO ]" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money." << std::endl << "You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl << std::endl;
 }
 
 void    Harl::warning(void)
 {
-    std::cout << "I think I deserve to have some extra bacon for free." << std::endl << "I've been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "[ WARNING ]" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free." << std::endl << "I've been coming for years whereas you started working here since last month." << std::endl << std::endl;
 }
 
 void    Harl::error(void)
 {
-    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+    std::cout << "[ ERROR ]" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
 }
 
 void    Harl::complain(std::string level)
@@ -44,33 +48,30 @@ void    Harl::complain(std::string level)
         &Harl::error
     };
 
-    int j = -1;
-    for (int i = 0;i < 4;i++)
+    int i = 0;
+    for (;i < 4 && in[i] != level;i++)
     {
-        if (in[i] == level)
-        {
-            j = i;
-            break ;
-        }
+        
     }
-    switch(j)
+    switch(i)
     {
         case 0:
             (this->*funcs[0])();
-            break ;
+            i++;
         case 1:
             (this->*funcs[1])();
-            break ;
+            i++;
         case 2:
             (this->*funcs[2])();
-            break ;
+            i++;
         case 3:
             (this->*funcs[3])();
+            i++;
             break ;
         default:
             msg();
             break ;
-    }  
+    }
 }
 
 Harl::Harl()
