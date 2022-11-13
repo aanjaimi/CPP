f78fca904ab0cba89a208222ae0da2150181fbc9
Ayoub Anjaimi <aanjaimi@e1r12p3.1337.ma>
2022-11-13 13:57:11 +0100

    D08/ex02:Donee

---
+++ D08/ex02/MutantStack.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <deque>
+#include <stack>
+#include <algorithm>
+#include <vector>
+
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{ 
+public:
+    MutantStack(){};
+    MutantStack(MutantStack const &other) : std::stack<T>(other){};
+    MutantStack const &operator = (MutantStack const &other){this->c = other.c;return (*this);};
+    ~MutantStack(){};
+    typedef typename std::stack<T>::container_type::iterator            iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator    rev_iterator;
+    iterator        begin(){return (this->c.begin());};
+    iterator        end(){return (this->c.end());};
+    rev_iterator    rbegin(){return (this->c.rbegin());};
+    rev_iterator    rend(){return (this->c.rend());};
+};
